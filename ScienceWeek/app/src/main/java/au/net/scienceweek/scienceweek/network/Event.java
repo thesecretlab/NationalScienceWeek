f27b37f2a452dd1201a4f0c806bce2144c61414f
Jon Manning <jon@secretlab.com.au>
2015-06-22 16:22:36 +1000

    Add network files, fix xml converter dependency

---
+++ ScienceWeek/app/src/main/java/au/net/scienceweek/scienceweek/network/Event.java
@@ -0,0 +1,74 @@
+package au.net.scienceweek.scienceweek.network;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+import java.net.URL;
+
+@Root(strict = false)
+public class Event {
+
+    @Override
+    public String toString() {
+        return EventName;
+    }
+
+    @Element(required = false)
+	public String EventID;
+
+    @Element(required = false)
+	String EventName;
+
+    @Element(required = false)
+	String EventType;
+
+    /*@ElementList(inline=true)
+    List<String> EventCategory;*/
+
+    @Element(required = false)
+    String EventStart;
+
+    @Element(required = false)
+	String EventEnd;
+
+    @Element(required = false)
+	String EventDescription;
+
+    @Element(required = false)
+	String EventTargetAudience;
+
+    @Element(required = false)
+	String EventPayment;
+
+    @Element(required = false)
+	boolean EventIsFree;
+
+    @Element(required = false)
+	String EventContactName;
+
+    @Element(required = false)
+	String EventContactOrganisation;
+
+    @Element(required = false)
+	String EventContactTelephone;
+
+    @Element(required = false)
+	String EventContactEmail;
+
+    @Element(required = false)
+	URL EventWebsite;
+
+    @Element(required = false)
+	String EventState;
+
+    @Element(required = false)
+	String EventMoreInfo;
+
+    @Element(required = false)
+    URL EventOfficialImageUrl;
+
+    @Element(required=false)
+    Venue Venue;
+  
+}
+
---
+++ ScienceWeek/app/src/main/java/au/net/scienceweek/scienceweek/network/EventServiceFactory.java
@@ -0,0 +1,42 @@
+package au.net.scienceweek.scienceweek.network;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.converter.SimpleXMLConverter;
+
+public class EventServiceFactory {
+    private static List<Event> events;
+
+    public static List<Event> getEvents() {
+        return events;
+    }
+
+    public static EventService createService() {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://www.scienceweek.net.au")
+                .setConverter(new SimpleXMLConverter())
+                .build();
+        return restAdapter.create(EventService.class);
+    }
+
+    public static void loadEvents(final Callback<Void> cb) {
+        createService().getEvents(new Callback<Events>() {
+            @Override
+            public void success(Events events, Response response) {
+                EventServiceFactory.events = events.events;
+                cb.success(null, response);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                cb.failure(error);
+            }
+        });
+    }
+
+
+}
