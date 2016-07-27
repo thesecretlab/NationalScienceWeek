711face3ba16dea553184f9edfc793a91e53d216
Jon Manning <jon@secretlab.com.au>
2016-07-27 13:04:33 +1000

    Update icon

--- NSWApp/Icon/resize.sh
+++ NSWApp/Icon/resize.sh
@@ -1,25 +1,57 @@
 ORIGINAL_ICON=$1
 
-convert $ORIGINAL_ICON -resize 1024 Icon-1024.png
-convert $ORIGINAL_ICON -resize 512 Icon-512.png
+function size {
+    convert "$ORIGINAL_ICON" -resize $1 "Icon-$2.png"
+}
 
-convert $ORIGINAL_ICON -resize 76 Icon-76.png
-convert $ORIGINAL_ICON -resize 152 Icon-76@2x.png
+size 1024 1024
+size 512 512
 
-convert $ORIGINAL_ICON -resize 72 Icon-72.png
-convert $ORIGINAL_ICON -resize 144 Icon-72@2x.png
+size 76 76
+size 152 76@2x
 
-convert $ORIGINAL_ICON -resize 60 Icon-60.png
-convert $ORIGINAL_ICON -resize 120 Icon-60@2x.png
+size 167 83.5@2x
 
-convert $ORIGINAL_ICON -resize 57 Icon-57.png
-convert $ORIGINAL_ICON -resize 114 Icon-57@2x.png
+size 72 72
+size 144 72@2x
+size 216 72@3x
 
-convert $ORIGINAL_ICON -resize 50 Icon-50.png
-convert $ORIGINAL_ICON -resize 100 Icon-50@2x.png
+size 120 60@2x
+size 180 60@3x
 
-convert $ORIGINAL_ICON -resize 40 Icon-40.png
-convert $ORIGINAL_ICON -resize 80 Icon-40@2x.png
+size 57 57
+size 114 57@2x
+size 171 57@3x
 
-convert $ORIGINAL_ICON -resize 29 Icon-29.png
-convert $ORIGINAL_ICON -resize 58 Icon-29@2x.png
+size 50 50
+size 100 50@2x
+
+size 40 40
+size 80 40@2x
+size 120 40@3x
+
+size 29 29
+size 58 29@2x
+size 87 29@3x
+
+size 120 120
+
+size 128 Hero
+size 256 Hero@2x
+size 384 Hero@3x
+
+size 48 AppleWatch-Notification-38mm
+size 55 AppleWatch-Notification-42mm
+
+size 58 AppleWatch-Companion-Settings@2x
+size 87 AppleWatch-Companion-Settings@3x
+
+size 80 AppleWatch-HomeScreen-All-LongLook-38mm
+size 88 AppleWatch-LongLook-42mm
+
+size 172 AppleWatch-ShortLook-38mm
+size 196 AppleWatch-ShortLook-42mm
+
+
+
+
