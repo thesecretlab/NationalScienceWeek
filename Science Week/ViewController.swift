de96fba327b24170ee0e895bbfc761bdeba3c2ce
Mars Geldard <github@mail.themartianlife.com>
2019-03-28 21:07:26 +1100

    Initial setup

---
+++ Science Week/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Science Week
+//
+//  Created by Mars Geldard on 28/3/19.
+//  Copyright © 2019 Mars Geldard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
