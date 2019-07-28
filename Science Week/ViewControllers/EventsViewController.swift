0a9775bff82b38565858cd292deb93d048c2870b
Jon Manning <jon@secretlab.com.au>
2019-07-28 12:17:57 +1000

    Add event grouping by date, searching

--- Science Week/ViewControllers/EventsViewController.swift
+++ Science Week/ViewControllers/EventsViewController.swift
@@ -10,39 +10,167 @@ import UIKit
 
 class EventsViewController: UITableViewController, EventDisplayingViewController {
     
-    @IBOutlet var parentView: UIView!
-    @IBOutlet weak var containerView: UIView!
-    @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var favouritesButton: UIBarButtonItem!
     
-    
-    @IBAction func favouritesButtonPressed(_ sender: Any) {
-        toggleFavouritesOnly()
+    enum ListType {
+        case allEvents, specifiedEvents
     }
     
-    private(set) var showFavouritesOnly = false
+    var listType : ListType = .allEvents
+    
+    
+    enum EventGrouping : Int {
+        case byDate, byTime
+    }
+    
+    typealias EventGroup = (start: Date, events: [Event])
+    
+    var events : [Event] = []
+    
+    var states : [String] = []
+    
+    var grouping : EventGrouping = .byDate
+    
+    var displayedEventGroups : [EventGroup] = []
+    
     private(set) var stateFilter: State? = nil
     
     private var displayedEvents: [Event] = EventsList.events
     
+    lazy var headerDateFormatter : DateFormatter = {
+        let d = DateFormatter()
+        
+        d.dateFormat = "EEEE, MMMM dd"
+        
+        return d
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setTheme()
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Events"
+        if #available(iOS 11.0, *) {
+            navigationItem.searchController = searchController
+        }
+        definesPresentationContext = true
+        
+        // Uncomment the following line to preserve selection between presentations
+        self.clearsSelectionOnViewWillAppear = true
     }
     
     func refresh() {
-        filterEvents(state: stateFilter, favouritesOnly: showFavouritesOnly)
+        events = EventsList.events
+        updateDisplayedEventGroups()
+        
+        tableView.reloadData()
     }
     
-    private func toggleFavouritesOnly() {
-        showFavouritesOnly = !showFavouritesOnly
-    
-        let statusMessage = showFavouritesOnly ? "Filtering by Favourites" : "Showing all"
-        showToast(message: statusMessage)
-        Logger.log(statusMessage)
+    func updateDisplayedEventGroups() {
         
-        favouritesButton.tintColor = showFavouritesOnly ? Theme.primaryAccentColour : Theme.inactiveColour
-        refresh()
+        let searchText : String?
+        
+        if let t = searchController.searchBar.text, searchController.isActive, t.count > 0 {
+            searchText = searchController.searchBar.text
+        } else {
+            searchText = nil
+        }
+        
+        let filteredEvents = events.filter({( event : Event) -> Bool in
+            
+            
+            // Do we have a search text?
+            if let searchText = searchText {
+                // Include it if the search text matches
+                let matches = event.name.lowercased().contains(searchText.lowercased())
+                
+                if matches == false {
+                    return false
+                }
+            }
+            
+            // Don't perform any more filtering if we're showing a subset of events
+            if listType == ListType.specifiedEvents {
+                return true
+            }
+            
+            // If a state is selected, and the event has a state, and the
+            // selected state is not the event's state, do not include it
+            if let state = self.stateFilter,
+                let eventState = event.state,
+                state != eventState {
+                return false
+            }
+            
+            // Does this event have an ID, and we're only showing favourites?
+            if Preferences.shared.displayMode == .favouritesOnly {
+                let isFavourite = FavouriteEvents.shared.isFavourite(id: event.id)
+                
+                if isFavourite == false {
+                    return false
+                }
+            }
+            
+            return true
+        })
+        
+        let groupedDict = Dictionary(grouping: filteredEvents, by: { (event: Event) -> Date in
+            
+            switch (grouping) {
+            case .byDate:
+                return Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: event.start ?? Date.distantFuture)!
+            case .byTime:
+                return event.start ?? Date.distantFuture
+            }
+        })
+        
+        displayedEventGroups = groupedDict.keys.sorted().map { (date) -> EventGroup in
+            return (start: date, events: groupedDict[date] ?? [])
+        }
+    }
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    @IBAction func favouritesButtonPressed(_ sender: Any) {
+
+        let currentDisplayMode = Preferences.shared.displayMode
+        
+        let newDisplayMode : Preferences.DisplayMode
+        
+        let statusMessage : String
+        
+        switch currentDisplayMode {
+            
+        case .all:
+            newDisplayMode = .favouritesOnly
+            statusMessage = "Filtering by Favourites"
+            
+            refresh()
+            
+        case .favouritesOnly:
+            newDisplayMode = .all
+            statusMessage = "Showing all"
+        }
+        
+        showToast(message: statusMessage)
+        
+        Preferences.shared.displayMode = newDisplayMode
+        
+        updateDisplayedEventGroups()
+        
+        tableView.reloadData()
+    }
+    
+    private func updateFavouritesButtonAppearance() {
+        switch Preferences.shared.displayMode {
+        case .all:
+            favouritesButton.tintColor = Theme.inactiveColour
+        case .favouritesOnly:
+            favouritesButton.tintColor = Theme.primaryAccentColour
+        }
     }
     
     private func setTheme() {
@@ -50,9 +178,11 @@ class EventsViewController: UITableViewController, EventDisplayingViewController
         self.navigationController?.navigationBar.tintColor = Theme.primaryAccentColour
         self.navigationController?.navigationBar.barStyle = Theme.lightTheme ? .default : .black
         
-        favouritesButton.tintColor = showFavouritesOnly ? Theme.primaryAccentColour : Theme.inactiveColour
+        updateFavouritesButtonAppearance()
         
-        parentView?.backgroundColor = Theme.primaryBackgroundColour
+        let bgView = UIView()
+        bgView.backgroundColor = Theme.primaryBackgroundColour
+        self.tableView.backgroundView = bgView
         
         tableView.backgroundColor = Theme.primaryBackgroundColour
     }
@@ -61,52 +191,48 @@ class EventsViewController: UITableViewController, EventDisplayingViewController
         return Theme.lightTheme ? UIStatusBarStyle.default : .lightContent
     }
 
-    internal func filterEvents(state: State?, favouritesOnly: Bool) {
-        var events = EventsList.events
-        
-        if let stateCode = state {
-            events = events.filter { event in event.state == stateCode || event.state == nil }
-        }
-        
-        if favouritesOnly {
-            events = events.filter { event in event.isFavourite }
-        }
-        
-        displayedEvents = events
-        tableView.reloadData()
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        guard let cell = sender as? UITableViewCell, let row = tableView.indexPath(for: cell)?.row else {
-            fatalError("Failed to get an event for cell \(sender)")
+        guard let cell = sender as? UITableViewCell, let indexPath = self.tableView.indexPath(for: cell) else {
+            fatalError("Segue sender is not a cell, or index path for cell not found")
         }
         
         guard let detailViewController = segue.destination as? EventDetailViewController else {
             fatalError("Segue expected destination to be an EventDetailViewController")
         }
         
-        detailViewController.event = displayedEvents[row]        
+        let event = self.displayedEventGroups[indexPath.section].events[indexPath.row]
+        
+        detailViewController.event = event
     }
 }
 
 // MARK: UITableView functions
 
 extension EventsViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return displayedEventGroups.count
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return displayedEvents.count
+        return displayedEventGroups[section].events.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headerDateFormatter.string(from: displayedEventGroups[section].start)
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let event = displayedEvents[indexPath.item]
-        let primary = "\(event.name)"
-        let secondary = "\(event.venue?.name  ?? "Event Venue"), \(event.state?.code ?? "Nationwide")"
         
         let cellIdentifier = "EventTableViewCell"
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) else {
-            fatalError("Failed to dequeue cell with identifier \(cellIdentifier)")
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        let event = displayedEventGroups[indexPath.section].events[indexPath.row]
+        
+        let primary = "\(event.name)"
+        let secondary = "\(event.venue?.name  ?? "Event Venue"), \(event.state?.code ?? "Nationwide")"
         
         cell.backgroundColor = .clear
         
@@ -117,5 +243,13 @@ extension EventsViewController {
         cell.detailTextLabel?.text = secondary
 
         return cell
+    }
+}
+
+extension EventsViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        updateDisplayedEventGroups()
+        tableView.reloadData()
     }
 }
